b4e67274aaff78752b1932697d0a3c46c72bd5e7
Jaimin Pandya <jaiminmoslake7020@gmail.com>
2024-08-14 15:59:35 -0700

    adding projects

---
+++ src/components/app-sections/ProjectBox.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+export type ProjectBoxPropTypes = {
+    projectName: string,
+    companyName: string,
+    projectUrl: string,
+    projectTech: string,
+    projectLogo: string,
+    logoAppearance: string
+};
+
+export type ProjectBoxEmptyPropTypes = {
+    setShowMore: Function,
+    showMore: boolean,
+};
+
+export const ProjectBoxEmpty = (props: ProjectBoxEmptyPropTypes) => {
+    const {
+        setShowMore, showMore
+    } = props;
+    return (
+        <div className={"project-box-wrapper"}>
+            <div className={"project-box empty-box "}>
+                <button type={"button"} onClick={() => {
+                    setShowMore(!showMore);
+                }}>{showMore ? "Show Less" : "Show More"}</button>
+            </div>
+        </div>
+    );
+}
+
+const ProjectBox = (props: ProjectBoxPropTypes) => {
+    const {
+        projectName,
+        companyName,
+        projectUrl,
+        projectTech,
+        projectLogo,
+        logoAppearance,
+    } = props;
+    return (
+        <div className={"project-box-wrapper group  "}>
+            <div className={"project-box lg:group-hover:before:animate-[shimmer_1s_forwards] "}>
+                <div className={"project-name"}>
+                    <h4>{projectName}</h4>
+                </div>
+                <div className={"project-url"}>
+                    <p>{projectUrl}</p>
+                </div>
+                <div className={"company-name"}>
+                    <p>{companyName}</p>
+                </div>
+                <div className={"project-tech"}>
+                    <p>{projectTech}</p>
+                </div>
+                <div className={`project-logo ${logoAppearance} `}>
+                    <a rel="noreferrer" href={projectUrl} target={"_blank"} >
+                        <img src={projectLogo}  alt={projectName} />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectBox;
--- src/components/app-sections/Projects.tsx
+++ src/components/app-sections/Projects.tsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ProjectBox, {ProjectBoxEmpty, ProjectBoxPropTypes} from './ProjectBox';
+import {useWindowSize} from '@uidotdev/usehooks';
+import {useScreenType} from '../../utils/utils';
+import emplaceLogo  from '../../assets/images/companies/emplace.svg';
+import umbracityLogo  from '../../assets/images/companies/umbracity.png';
+
+const projects = [
+    {
+        projectName: "Owner Platform ",
+        companyName: "Emplace Technology Inc",
+        projectUrl: "https://owner.emplace.com/",
+        projectTech: "React, TypeScript, Rest API, Redux",
+        projectLogo: emplaceLogo,
+        logoAppearance: "white-text"
+    }
+] as ProjectBoxPropTypes[];
 
 const Projects = () => {
+
+    const { width } = useWindowSize();
+    const type = useScreenType( width || 0 );
+    const gridSizeObject = {
+        "xs": 1,
+        "sm": 1,
+        "md": 3,
+        "lg": 3,
+        "xl": 5,
+        "2xl": 5,
+    };
+
+    const [showMore, setShowMore] = useState<boolean>(false);
+
     return (
         <section className={"section section-projects"} >
-            <h1>Projects</h1>
+            <h1>Experience</h1>
+            <div className={"section-content section-projects-content"}>
+                <div className={"projects-wrapper"}>
+                    {
+                        showMore && projects.map((project) => <ProjectBox key={project.projectName} {...project} />)
+                    }
+                    {
+                        !showMore && projects.filter((v, i) => i < gridSizeObject[type || "xl"]).map((project) => <ProjectBox key={project.projectName} {...project} />)
+                    }
+                    <ProjectBoxEmpty key={"empty"} showMore={showMore} setShowMore={(v:boolean) => {
+                        setShowMore(v);
+                        if (!v) {
+                            const item = document.querySelector('.section.section-projects');
+                            if (item) {
+                                item.scrollIntoView({behavior: 'smooth'})
+                            }
+                        }
+                    }} />
+                </div>
+            </div>
         </section>
     );
 }
