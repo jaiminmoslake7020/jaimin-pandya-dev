d4d246e0b241f0a8fffe21bc116cc6ded54b1077
Jaimin Pandya <jaiminmoslake7020@gmail.com>
2024-08-15 13:35:43 -0700

    adding projects

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback, useEffect} from 'react';
 
 export type NavPropTypes = {
     showSidebar?: Function
@@ -28,19 +28,39 @@ const menuLinks = [
 ];
 
 const Nav = ({showSidebar}: NavPropTypes) => {
+
+    const moveContent = useCallback((content:string) => {
+        const item = document.querySelector('.section.section-'+content);
+        if (item) {
+            item.scrollIntoView({ behavior: 'smooth' });
+            if (showSidebar) {
+                showSidebar(false);
+            }
+        }
+    }, [showSidebar])
+
+    useEffect(() => {
+        const handleWindowLoad = () => {
+            const hash = window.location.hash.replaceAll("#", "");
+            moveContent(hash);
+        };
+
+        // Add the event listener for window load
+        window.addEventListener('load', handleWindowLoad);
+
+        // Clean up the event listener on component unmount
+        return () => {
+            window.removeEventListener('load', handleWindowLoad);
+        };
+    }, [moveContent])
+
     return (
         <nav className={"nav nav-main"}>
             {
                 menuLinks.map(({label, content}) => <a key={label} href={content} className={"nav-link"}
                     onClick={(e) => {
                         e.preventDefault();
-                        const item = document.querySelector('.section.section-'+content);
-                        if (item) {
-                           item.scrollIntoView({ behavior: 'smooth' });
-                           if (showSidebar) {
-                               showSidebar(false);
-                           }
-                        }
+                        moveContent(content);
                     }}
                 >{label}</a>)
             }
