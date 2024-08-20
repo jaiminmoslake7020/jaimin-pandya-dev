b392676e5d02026dc69a1298bd911f74b2e47f3e
Jaimin Pandya <jaiminmoslake7020@gmail.com>
2024-08-20 14:12:15 -0700

    removing unused useEffect

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useRef, useState} from 'react';
 import FaIcon from './FaIcon';
 import {IconProp} from '@fortawesome/fontawesome-svg-core';
 
